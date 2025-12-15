a9f904a136c91789c5813512da7eb0aeb8bb9c1b
Vegenakki27 <123238177+Vegenakki27@users.noreply.github.com>
2025-12-15 15:59:15 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,8 +1,8 @@
+// login.js
 function login() {
   const pass = document.getElementById("passwordInput").value;
-
   if (pass === "1234") {
-    window.location.href = "index.html";
+    window.location.href = "dashboard.html"; // vanha index.html
   } else {
     document.getElementById("error").textContent = "Väärä salasana";
   }
